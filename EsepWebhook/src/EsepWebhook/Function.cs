efa2c8a54c098a06475383a1591174ec2e79dbff
James Ulaszek <jaminthoseconcerts@gmail.com>
2022-10-16 23:51:44 -0400

    removed the link and added using statements

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.Lambda.Core;
+using System.Text;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -21,7 +23,7 @@ public class Function
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, "{do not check in this URL}")
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, "{not committing the link}")
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
